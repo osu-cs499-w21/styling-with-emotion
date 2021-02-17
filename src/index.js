d08aa5b9fa080cbfbf02972bd4cc71ec989acaf7
Rob Hess <hessro@oregonstate.edu>
2021-02-17 12:16:09 -0800

    Add code from 2/10 lecture.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import App from './App';
+import { Global, css } from '@emotion/react';
+
+// import App from './App';
+import App from './AppWithStyledComponents';
+
+const globalStyles = css`
+  @import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro&display=swap');
+  body {
+    font-family: 'Source Sans Pro', sans-serif;
+  }
+`;
 
 ReactDOM.render(
   <React.StrictMode>
+    <Global styles={globalStyles} />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
